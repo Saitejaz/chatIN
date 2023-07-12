cdf36bc07dacb7eefd6745c4c6901e5fc26bb484
Saitejaz <saitejamudhulkar9@gmail.com>
2023-07-12 22:57:31 +0530

    New users are able to Register through postman

---
+++ src/main/java/com/chatapp/main/Auth/Registration/User.java
@@ -0,0 +1,40 @@
+package com.chatapp.main.Auth.Registration;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+	
+	@Id
+	private String id;
+	private String userName;
+	private String userEmail;
+	private String password;
+    private String registeredDate;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRegisteredDate() {
+		return registeredDate;
+	}
+	public void setRegisteredDate(String registeredDate) {
+		this.registeredDate = registeredDate;
+	}
+	
+}
---
+++ src/main/java/com/chatapp/main/Auth/Registration/UserController.java
@@ -0,0 +1,25 @@
+package com.chatapp.main.Auth.Registration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	private final UserRepository userRepository;
+	
+	@Autowired
+	public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+	}
+	
+	@PostMapping("/register")
+	public User createUser(@RequestBody User user) {
+		System.out.println(user.getPassword());
+		return userRepository.save(user);
+	}
+}
